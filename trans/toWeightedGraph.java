0023aff416703172f818de36dbf051e36e17e837
Nikolay Korshunov <nikolay.korshunov@intel.com>
2016-05-11 11:10:18 +0300

    updated toWeightedgraph.java It makes several files in directory with graphs we need to calculate maximum clique in and also creates a dictionaru with pairs number - SECID

--- trans/toWeightedGraph.java
+++ trans/toWeightedGraph.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import org.apache.log4j.Level;
@@ -14,10 +18,12 @@ import org.jetel.metadata.DataRecordMetadata;
  * This is an example custom transformer. It shows how you can
  *  read records, process their values and write records.
  */
+
+
 public class toWeightedGraph extends AbstractGenericTransform {
 
 	@Override
-	public void execute() {
+	public void execute() throws IOException {
 		/** This code is an example. Replace it with your custom code. */
 
 		/** This is how you can read component properties. */
@@ -46,10 +52,12 @@ public class toWeightedGraph extends AbstractGenericTransform {
 		}
 		*/
 		
+		String RESULT_DIR="C:\\_course_work\\course_work\\toGraphs\\";
+		
 		ArrayList<Double> limits=new ArrayList<Double>();
 		
 		//filling the list of limits in my way
-		double limit=0.2;
+		double limit=-0.5;
 		while (limit<1){
 			limits.add(limit);
 			limit+=0.15;
@@ -86,12 +94,20 @@ public class toWeightedGraph extends AbstractGenericTransform {
 		}
 		System.out.println("READ FINISHED");
 		
-		
-		
+		PrintWriter pw2=new PrintWriter(RESULT_DIR+"dict.csv");
+		for (int h=0; h<secids.length; h++){
+			pw2.println(h+";"+secids[h]);
+		}
+		pw2.close();
 		
 		DataRecord outRecord=outRecords[0];
 		
 		for (double lim:limits){
+			File f=new File(RESULT_DIR+lim+"_graph.graph");
+			f.createNewFile();
+			PrintWriter pw=new PrintWriter(f);
+			System.out.println("START");;
+			ArrayList<String> edges=new ArrayList<String>();
 			outRecord.getField(0).setValue(title);
 			writeRecordToPort(0, outRecord);
 			for (int i=0; i<n-1;i++){
@@ -99,8 +115,10 @@ public class toWeightedGraph extends AbstractGenericTransform {
 				//if (fields[i].getValue()!=null)
 					//s=fields[i].getValue().toString();
 				for (int k=0; k<n-1; k++)
-					if (cor_matrix[i][k]>lim)
+					if (cor_matrix[i][k]>lim){
 						s+=";1";
+						edges.add("e "+i+" "+k);
+					}
 					else
 						s+=";0";
 				outRecord.getField(0).setValue(s);
@@ -108,11 +126,21 @@ public class toWeightedGraph extends AbstractGenericTransform {
 				
 			}
 			outRecord.getField(0).setValue("LIMIT "+lim+" ENDS");
-			writeRecordToPort(0, outRecord);		
+			writeRecordToPort(0, outRecord);	
+			
+			pw.println("p edge "+(n-1)+" "+edges.size());
+			for (String s:edges){
+				pw.println(s);
+			}
+			pw.close();
+			
 		}
 		//end filling
 		
 	}
+	
+	
+
 
 	@Override
 	public ConfigurationStatus checkConfig(ConfigurationStatus status) {
